a5b1c0d1cee9875031d31ab3c9ffdc8e153a00ff
Ashik Hasan <120168546+Ashik-gf@users.noreply.github.com>
2024-04-02 01:00:18 +0600

    uplode this file

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function handelButton(){
-    const par =prompt("Enter Text hare:")
-    document.getElementById('par').innerText=par
-document.getElementById('tag').innerHTML= par.trim(' ');
-}
- 
+const input = document.querySelectorAll("button");
+const output = document.querySelector("input");
+console.log(input.innerText);
+let sum = "";
+input.forEach((button) => {
+  button.addEventListener("click", function () {
+    let inputValue = button.innerText;
+    if (inputValue == "AC") {
+      sum = "";
+      output.value = sum;
+    } else if (inputValue == "DC") {
+      sum = sum.substring(0, sum.length - 1);
+      output.value = sum;
+    } else if(inputValue == "="){
+sum = eval(sum);
+output.value= sum;
+    }else{
+        sum = sum + inputValue;
+        output.value = sum;
+    }
+   
+    console.log(sum);
+  });
+});
